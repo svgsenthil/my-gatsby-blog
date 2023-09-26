8a61c5ea158547ae6ff1b6aa64782e9d72ceac1e
Senthil Kumar <svgsenthil@gmail.com>
2023-09-26 21:24:09 +0530

    Added Netlify Configration for Deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,12 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+const adapter = require("gatsby-adapter-netlify")
+
 module.exports = {
+  adapter: adapter({
+    excludeDatastoreFromEngineFunction: false,
+  }),
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
